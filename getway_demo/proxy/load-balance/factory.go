416c9886c2e30a403317384ac966750fea399e98
DoubleX <double@tg10010.com>
2020-07-02 10:06:52 +0700

    add:hash一致性负载均衡策略代码实现。以及实际应用实例

---
+++ getway_demo/proxy/load-balance/factory.go
@@ -0,0 +1,26 @@
+package load_balance
+
+// 定义 负载均衡 枚举
+type LbType int
+
+const (
+	LbRandom LbType = iota
+	LbRoundRobin
+	LbWeightRoundRobin
+	LbConsistentHash
+)
+
+func LoadBalanceFactory(lbType LbType) LoadBalance {
+	switch lbType {
+	case LbRandom:
+		return &RandomBalance{}
+	case LbRoundRobin:
+		return &RoundRobinBalance{}
+	case LbWeightRoundRobin:
+		return &WeightRoundRobinBalance{}
+	case LbConsistentHash:
+		return NewConsistentHashBalance(nil, 10)
+	default:
+		return &RandomBalance{}
+	}
+}
--- getway_demo/proxy/load-balance/loadBalanceInterface.go
+++ getway_demo/proxy/load-balance/loadBalanceInterface.go
@@ -3,5 +3,4 @@ package load_balance
 type LoadBalance interface {
 	Add(...string) error
 	Get(string) (string, error)
-	Next() string
 }
