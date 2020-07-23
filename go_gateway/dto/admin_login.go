0634255d76dda11dc5e25b8e8e96cd9b98fc6aaa
DoubleX <double@tg10010.com>
2020-07-23 09:35:21 +0700

    add:参数校验示例

--- go_gateway/dto/admin_login.go
+++ go_gateway/dto/admin_login.go
@@ -6,8 +6,8 @@ import (
 )
 
 type AdminLoginInput struct {
-	UserName string `json:"username" form:"username" comment:"管理员用户名" example:"admin" validate:"required"` //管理员用户名
-	Password string `json:"password" form:"password" comment:"密码" example:"123456" validate:"required"`    //密码
+	UserName string `json:"username" form:"username" comment:"管理员用户名" example:"admin" validate:"required,valid_username"` //管理员用户名
+	Password string `json:"password" form:"password" comment:"密码" example:"123456" validate:"required"`                   //密码
 }
 
 func (param *AdminLoginInput) BindValidParam(c *gin.Context) error {
--- go_gateway/middleware/translation.go
+++ go_gateway/middleware/translation.go
@@ -45,16 +45,16 @@ func TranslationMiddleware() gin.HandlerFunc {
 
 			//自定义验证方法
 			//https://github.com/go-playground/validator/blob/v9/_examples/custom-validation/main.go
-			val.RegisterValidation("is-validuser", func(fl validator.FieldLevel) bool {
+			val.RegisterValidation("valid_username", func(fl validator.FieldLevel) bool {
 				return fl.Field().String() == "admin"
 			})
 
 			//自定义验证器
 			//https://github.com/go-playground/validator/blob/v9/_examples/translations/main.go
-			val.RegisterTranslation("is-validuser", trans, func(ut ut.Translator) error {
-				return ut.Add("is-validuser", "{0} 填写不正确哦", true)
+			val.RegisterTranslation("valid_username", trans, func(ut ut.Translator) error {
+				return ut.Add("valid_username", "{0} 填写不正确哦 {1}", true)
 			}, func(ut ut.Translator, fe validator.FieldError) string {
-				t, _ := ut.T("is-validuser", fe.Field())
+				t, _ := ut.T("valid_username", fe.Field(), fe.Param())
 				return t
 			})
 			break
